d090dee8ab88e85f1768899292df283d4ae5bd5d
omaraltali <oaltali.e@gmail.com>
2024-05-25 03:57:20 +0200

    add Color Theme extensions

---
+++ CryptoTracker/CryptoTracker/Extensions/Color.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  CryptoTracker
+//
+//  Created by Omar Altali on 25/05/2024.
+//
+
+import SwiftUI
+
+extension Color {
+
+    static let theme = ColorTheme()
+}
+
+
+struct ColorTheme {
+
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+
+}
