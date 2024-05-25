959753b7f8d3b821b5379382e2197e73b72c7f37
omaraltali <oaltali.e@gmail.com>
2024-05-25 05:22:35 +0200

    Apply header section in homeView

--- CryptoTracker/CryptoTracker/Core/Home/Views/HomeView.swift
+++ CryptoTracker/CryptoTracker/Core/Home/Views/HomeView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct HomeView: View {
+
+    @State private var showProtfolio: Bool = false
+
     var body: some View {
         ZStack {
             Color.theme.background.ignoresSafeArea()
 
+            // content
             VStack {
-                Text("Header")
+                HStack {
+                    CircleButtonView(iconeName: showProtfolio ? "plus" : "info")
+                        .background(
+                            CircleButtonAnimationView(animate: $showProtfolio)
+                        )
+                    Spacer()
+                    Text(showProtfolio ? "Portfolio" : "Live Prices")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.theme.accent)
+                    Spacer()
+                    CircleButtonView(iconeName: "chevron.right")
+                        .rotationEffect(.degrees(showProtfolio ? 180 : 0))
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                showProtfolio.toggle()
+                            }
+                        }
+                }
+                .padding(.horizontal)
                 Spacer(minLength: 0)
             }
         }
