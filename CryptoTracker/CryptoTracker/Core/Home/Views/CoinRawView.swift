25890f1095a4a780a06c438d66cfc42a072c4a3a
omaraltali <oaltali.e@gmail.com>
2024-05-25 16:10:38 +0200

    create CoinRawView

---
+++ CryptoTracker/CryptoTracker/Core/Home/Views/CoinRawView.swift
@@ -0,0 +1,21 @@
+//
+//  CoinRawView.swift
+//  CryptoTracker
+//
+//  Created by Omar Altali on 25/05/2024.
+//
+
+import SwiftUI
+
+struct CoinRawView: View {
+
+    let coin: CoinModel
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinRawView()
+}
