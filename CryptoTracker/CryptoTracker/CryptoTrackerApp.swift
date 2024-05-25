7df0212e9616efa4597300b2c5b7574e26f6ea65
omaraltali <oaltali.e@gmail.com>
2024-05-25 03:27:05 +0200

    init project

---
+++ CryptoTracker/CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Omar Altali on 25/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
