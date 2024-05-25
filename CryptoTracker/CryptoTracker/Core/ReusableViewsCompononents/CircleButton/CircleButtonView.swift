de57d51351a00f2d5c44a253c1e989d284144c20
omaraltali <oaltali.e@gmail.com>
2024-05-25 05:22:15 +0200

    Update CircleButtonView.swift

--- CryptoTracker/CryptoTracker/Core/ReusableViewsCompononents/CircleButton/CircleButtonView.swift
+++ CryptoTracker/CryptoTracker/Core/ReusableViewsCompononents/CircleButton/CircleButtonView.swift
@@ -8,15 +8,29 @@
 import SwiftUI
 
 struct CircleButtonView: View {
+    let iconeName: String
+
     var body: some View {
-        Image(systemName: "heart.fill")
+        Image(systemName: iconeName)
+            .font(.headline)
+            .foregroundColor(Color.theme.accent)
+            .frame(width: 50, height: 50)
+            .background(
+                Circle()
+                    .foregroundColor(Color.theme.background)
+                )
+                    .shadow(
+                        color: Color.theme.accent.opacity(0.25),
+                        radius: 10, x: 0, y: 0)
+            .padding()
     }
 }
 
 
 struct CircleButtonView_Previews: PreviewProvider {
     static var previews: some View {
-        CircleButtonView()
+        CircleButtonView(iconeName: "info")
             .previewLayout(.sizeThatFits)
+            .colorScheme(.dark)
     }
 }
