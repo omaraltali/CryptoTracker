77e7eff1802747d550688fc26aecba39f17d36d3
omaraltali <oaltali.e@gmail.com>
2024-05-25 04:17:34 +0200

    Create CircleButtonView.swift

---
+++ CryptoTracker/CryptoTracker/Core/ReusableViewsCompononents/CircleButton/CircleButtonView.swift
@@ -0,0 +1,22 @@
+//
+//  CircleButtonView.swift
+//  CryptoTracker
+//
+//  Created by Omar Altali on 25/05/2024.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Image(systemName: "heart.fill")
+    }
+}
+
+
+struct CircleButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleButtonView()
+            .previewLayout(.sizeThatFits)
+    }
+}
