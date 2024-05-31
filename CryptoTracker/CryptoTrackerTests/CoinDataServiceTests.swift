8b4ecc8ca72875e8cb139686525c5b48ffd6073a
omaraltali <oaltali.e@gmail.com>
2024-06-01 02:34:39 +0300

    unit test CoinDataService --> testGetCoinsSuccess Pass

---
+++ CryptoTracker/CryptoTrackerTests/CoinDataServiceTests.swift
@@ -0,0 +1,51 @@
+//
+//  CoinDataServiceTests.swift
+//  CryptoTrackerTests
+//
+//  Created by Omar Altali on 31/05/2024.
+//
+
+import XCTest
+import Combine
+@testable import CryptoTracker
+
+final class CoinDataServiceTests: XCTestCase {
+
+    private var cancellables: Set<AnyCancellable>!
+
+    override func setUp() {
+        super.setUp()
+        cancellables = []
+    }
+
+    override func tearDown() {
+        cancellables = nil
+        super.tearDown()
+    }
+
+    func testGetCoinsSuccess() {
+        let coins = [CoinModel(id: "bitcoin", symbol: "btc", name: "Bitcoin", image: "", currentPrice: 50000, marketCap: 1000000, marketCapRank: 1, fullyDilutedValuation: nil, totalVolume: 50000, high24H: 60000, low24H: 40000, priceChange24H: 5000, priceChangePercentage24H: 10, marketCapChange24H: 100000, marketCapChangePercentage24H: 10, circulatingSupply: 10000, totalSupply: 10000, maxSupply: 21000, ath: 60000, athChangePercentage: -10, athDate: "", atl: 100, atlChangePercentage: 49000, atlDate: "", lastUpdated: "", sparklineIn7D: nil, priceChangePercentage24HInCurrency: nil, currentHoldings: nil)]
+        let fetchDataResult = Just(coins)
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+
+        let dataService = makeSUT(fetchDataResult: fetchDataResult)
+
+        let expectation = XCTestExpectation(description: "All coins are published")
+        dataService.$allCoins
+            .sink { receivedCoins in
+                XCTAssertEqual(receivedCoins.count, 1)
+                XCTAssertEqual(receivedCoins.first?.name, "Bitcoin")
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+
+        wait(for: [expectation], timeout: 1.0)
+    }
+    private func makeSUT(fetchDataResult: AnyPublisher<[CoinModel], Error>) -> CoinDataService {
+        let mockNetworkManager = MockNetworkManager()
+        mockNetworkManager.fetchDataResult = fetchDataResult
+        let dataService = CoinDataService(networkManager: mockNetworkManager)
+        return dataService
+    }
+}
