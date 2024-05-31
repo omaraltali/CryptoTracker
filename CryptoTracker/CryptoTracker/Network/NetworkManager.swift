890ff111ad165147af3d0019990c29640e128fc5
omaraltali <oaltali.e@gmail.com>
2024-05-31 23:29:01 +0300

    create generic reusable network layer

---
+++ CryptoTracker/CryptoTracker/Network/NetworkManager.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkManager.swift
+//  CryptoTracker
+//
+//  Created by Omar Altali on 31/05/2024.
+//
+
+import Foundation
+import Combine
+
+class NetworkManager: NetworkManagerProtocol {
+
+    func fetchData<T: Decodable>(from url: URL, type: T.Type) -> AnyPublisher<T, Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap { (data, response) in
+                guard let response = response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                return data
+            }
+            .decode(type: T.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
+
