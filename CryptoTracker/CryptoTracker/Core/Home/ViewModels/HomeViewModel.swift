156d66a3b3bb23799a041a1bd0fe990e78a5c1b7
omaraltali <oaltali.e@gmail.com>
2024-05-31 23:33:42 +0300

    update home viewModel to user CoinDataService

--- CryptoTracker/CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoTracker/CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -6,10 +6,26 @@
 //
 
 import Foundation
-
+import Combine
 
 class HomeViewModel: ObservableObject {
 
-    
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    private var cancellables: Set<AnyCancellable> = []
 
+    let dataService: CoinDataService
+
+    init(dataService: CoinDataService) {
+        self.dataService = dataService
+        addSubscribers()
+    }
+
+    func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] receivedCoins in
+                self?.allCoins = receivedCoins
+            }
+            .store(in: &cancellables)
+    }
 }
