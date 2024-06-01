fe4ded17a24a7f559192a3139a14ac088c7eeba7
omaraltali <oaltali.e@gmail.com>
2024-06-01 17:36:19 +0300

    update homeViewModel to subscribe then publish market data + enhance and seperate filtering and mapping to separate  functions to make testing life easier when the time comes

--- CryptoTracker/CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoTracker/CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -10,15 +10,19 @@ import Combine
 
 class HomeViewModel: ObservableObject {
 
+    @Published var statistics: [StatisticModel] = []
+
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     @Published var searchText: String = ""
     private var cancellables: Set<AnyCancellable> = []
 
-    let dataService: CoinDataService
+    private let coinDataService: CoinDataService
+    private let marketDataService: MarketDataService
 
-    init(dataService: CoinDataService) {
-        self.dataService = dataService
+    init(dataService: CoinDataService, marketDataService: MarketDataService) {
+        self.coinDataService = dataService
+        self.marketDataService = marketDataService
         addSubscribers()
     }
 
@@ -35,23 +39,49 @@ class HomeViewModel: ObservableObject {
         // Subscribe to seachText :
         // and make the searchText also subscribe to data service $allCoins using combineLatest
         $searchText
-            .combineLatest(dataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins)
             .debounce(for: .milliseconds(300), scheduler: RunLoop.main)
-            .map { text, initialCoins -> [CoinModel] in
-                guard !text.isEmpty else {
-                    return initialCoins
-                }
-
-                let lowercasedText = text.lowercased()
-               return initialCoins.filter {
-                    $0.name.lowercased().contains(lowercasedText) ||
-                    $0.symbol.lowercased().contains(lowercasedText) ||
-                    $0.id.lowercased().contains(lowercasedText)
-                }
-            }
+            .map(filterCoins)
             .sink {[weak self] filteredCoinsReturned in
                 self?.allCoins = filteredCoinsReturned
             }
             .store(in: &cancellables)
+
+
+        marketDataService.$marketData.map(mapMarketDataToStatistics)
+        .sink {[weak self] arrayOfStats in
+            self?.statistics = arrayOfStats
+        }
+        .store(in: &cancellables)
     }
+
+    private func filterCoins(searchText: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !searchText.isEmpty else {
+            return coins
+        }
+
+        let lowercasedText = searchText.lowercased()
+        return coins.filter {
+            $0.name.lowercased().contains(lowercasedText) ||
+            $0.symbol.lowercased().contains(lowercasedText) ||
+            $0.id.lowercased().contains(lowercasedText)
+        }
+    }
+
+    private func mapMarketDataToStatistics(_ marketData: MarketDataModel?) -> [StatisticModel] {
+        guard let data = marketData else {
+            return []
+        }
+
+        let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+        let volume = StatisticModel(title: "24h Volume", value: data.volume)
+        let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+        let portfolio = StatisticModel(title: "Portfolio Value", value: "$0.00", percentageChange: 0)
+
+        return [marketCap, volume, btcDominance, portfolio]
+    }
+
+
+
+
 }
