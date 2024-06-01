9d0b5d34da922143863f3091714cae5aa64e1f40
omaraltali <oaltali.e@gmail.com>
2024-06-01 05:26:37 +0300

    make searchText Binding instead of @State to make it reusable anywhere in the app

--- CryptoTracker/CryptoTracker/Core/ReusableViewsCompononents/SearchBarView.swift
+++ CryptoTracker/CryptoTracker/Core/ReusableViewsCompononents/SearchBarView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SearchBarView: View {
 
-    @State var searchText: String = ""
+    @Binding var searchText: String
 
     var body: some View {
         HStack {
@@ -43,5 +43,5 @@ struct SearchBarView: View {
 }
 
 #Preview {
-    SearchBarView()
+    SearchBarView(searchText: .constant(""))
 }
