d7608b2627f72adf3802dfae10decf22277aadff
omaraltali <oaltali.e@gmail.com>
2024-06-01 17:35:18 +0300

    create MarketData Service to getMarketData using same networkLayer

---
+++ CryptoTracker/CryptoTracker/Network/Services/MarketDataService.swift
@@ -0,0 +1,34 @@
+//
+//  MarketDataService.swift
+//  CryptoTracker
+//
+//  Created by Omar Altali on 01/06/2024.
+//
+
+import Foundation
+import Combine
+
+class MarketDataService {
+
+    @Published var marketData: MarketDataModel? = nil
+    var marketDatasubscription: AnyCancellable?
+    private let networkManager: NetworkManagerProtocol
+
+    init(networkManager: NetworkManagerProtocol) {
+        self.networkManager = networkManager
+        getMarketData()
+    }
+
+    private func getMarketData() {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
+        marketDatasubscription = networkManager.fetchData(from: url, type: GlobalData.self)
+            .sink { completion in
+                if case let .failure(error) = completion {
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { [weak self] marketData in
+                self?.marketData = marketData.data
+                self?.marketDatasubscription?.cancel()
+            }
+    }
+}
