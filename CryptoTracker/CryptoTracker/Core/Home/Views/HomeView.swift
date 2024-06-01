a2a7faf24315c5501bc55934a611993bfc7aad32
omaraltali <oaltali.e@gmail.com>
2024-06-01 17:37:59 +0300

    followup for previous commit

--- CryptoTracker/CryptoTracker/Core/Home/Views/HomeView.swift
+++ CryptoTracker/CryptoTracker/Core/Home/Views/HomeView.swift
@@ -18,6 +18,8 @@ struct HomeView: View {
             VStack {
                 homeHeader
 
+                HomeStatsView(showPortfolio: $showProtfolio)
+
                 SearchBarView(searchText: $viewModel.searchText)
 
                 coloumnTitles
