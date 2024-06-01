f4ddbc6cc6f78a1fa474ecf84002a37cd3e566b4
omaraltali <oaltali.e@gmail.com>
2024-06-01 06:08:59 +0300

    implement filtering functionality with COMBINE POWER

--- CryptoTracker/CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoTracker/CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -23,9 +23,36 @@ class HomeViewModel: ObservableObject {
     }
 
     func addSubscribers() {
-        dataService.$allCoins
-            .sink { [weak self] receivedCoins in
-                self?.allCoins = receivedCoins
+
+        // MARK: Combine POWER
+        // we get rid of this first subscribing since we combined sinking on $searchText and dataService.$allCoins
+//        dataService.$allCoins
+//            .sink { [weak self] receivedCoins in
+//                self?.allCoins = receivedCoins
+//            }
+//            .store(in: &cancellables)
+
+
+
+
+        // Subscribe to seachText :
+        // and make the searchText also subscribe to data service $allCoins using combineLatest
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .map { text, initialCoins -> [CoinModel] in
+                guard !text.isEmpty else {
+                    return initialCoins
+                }
+
+                let lowercasedText = text.lowercased()
+               return initialCoins.filter {
+                    $0.name.lowercased().contains(lowercasedText) ||
+                    $0.symbol.lowercased().contains(lowercasedText) ||
+                    $0.id.lowercased().contains(lowercasedText)
+                }
+            }
+            .sink {[weak self] filteredCoinsReturned in
+                self?.allCoins = filteredCoinsReturned
             }
             .store(in: &cancellables)
     }
