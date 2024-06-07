7fca696924a91ceda4703770c7ca79706419432d
omaraltali <oaltali.e@gmail.com>
2024-06-07 06:31:33 +0400

    create coinLogoView

---
+++ CryptoTracker/CryptoTracker/Core/ReusableViewsCompononents/CoinLogoView.swift
@@ -0,0 +1,40 @@
+//
+//  CoinLogoView.swift
+//  CryptoTracker
+//
+//  Created by Omar Altali on 07/06/2024.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct CoinLogoView: View {
+    let coin: CoinModel
+
+    var body: some View {
+        VStack {
+            KFImage(URL(string: coin.image))
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundStyle(Color.theme.accent)
+                .lineLimit(1)
+                .minimumScaleFactor(0.5)
+            Text(coin.name)
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+                .lineLimit(2)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+
+        }
+    }
+}
+
+struct CoinLogoView_Preview: PreviewProvider {
+    static var previews: some View {
+        CoinLogoView(coin: dev.coin)
+            .previewLayout(.sizeThatFits)
+    }
+}
