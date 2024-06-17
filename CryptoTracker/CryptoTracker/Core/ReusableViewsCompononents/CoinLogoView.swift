aa0eac8fc8d381718b7c6f1e867887530ff3f924
omaraltali <oaltali.e@gmail.com>
2024-06-17 07:16:14 +0400

    spesific need for detailView to only show image ..

--- CryptoTracker/CryptoTracker/Core/ReusableViewsCompononents/CoinLogoView.swift
+++ CryptoTracker/CryptoTracker/Core/ReusableViewsCompononents/CoinLogoView.swift
@@ -9,24 +9,32 @@ import SwiftUI
 import Kingfisher
 
 struct CoinLogoView: View {
+    @State var isDetailView: Bool = false
     let coin: CoinModel
 
     var body: some View {
-        VStack {
+        if !isDetailView {
+            VStack {
+                KFImage(URL(string: coin.image))
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                Text(coin.symbol.uppercased())
+                    .font(.headline)
+                    .foregroundStyle(Color.theme.accent)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.5)
+                Text(coin.name)
+                    .font(.caption)
+                    .foregroundStyle(Color.theme.secondaryText)
+                    .lineLimit(2)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+
+            }
+        } else {
             KFImage(URL(string: coin.image))
                 .resizable()
-                .frame(width: 50, height: 50)
-            Text(coin.symbol.uppercased())
-                .font(.headline)
-                .foregroundStyle(Color.theme.accent)
-                .lineLimit(1)
-                .minimumScaleFactor(0.5)
-            Text(coin.name)
-                .font(.caption)
-                .foregroundStyle(Color.theme.secondaryText)
-                .lineLimit(2)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
+//                .frame(width: 50, height: 50)
 
         }
     }
