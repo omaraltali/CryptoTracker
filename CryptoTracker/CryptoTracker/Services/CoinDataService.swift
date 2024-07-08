927b7a4d8ba4e9987ff99adb06d09ee22729bf95
omaraltali <oaltali.e@gmail.com>
2024-07-08 20:06:28 +0300

    keep old coinDataService as a comment

--- CryptoTracker/CryptoTracker/Services/CoinDataService.swift
+++ CryptoTracker/CryptoTracker/Services/CoinDataService.swift
@@ -32,3 +32,30 @@ class CoinDataService: CoinDataServiceProtocol {
             .eraseToAnyPublisher()
     }
 }
+
+//
+//
+//class CoinDataService {
+//
+//    @Published var allCoins: [CoinModel] = []
+//    var coinSubscription: AnyCancellable?
+//    private let networkManager: NetworkManagerProtocol
+//
+//    init(networkManager: NetworkManagerProtocol) {
+//        self.networkManager = networkManager
+//        getCoins()
+//    }
+//
+//    func getCoins() {
+//        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
+//        coinSubscription = networkManager.fetchData(from: url, type: [CoinModel].self)
+//            .sink { completion in
+//                if case let .failure(error) = completion {
+//                    print(error.localizedDescription)
+//                }
+//            } receiveValue: { [weak self] coins in
+//                self?.allCoins = coins
+//                self?.coinSubscription?.cancel()
+//            }
+//    }
+//}
